ffb1343a8158a6b18d0ff3239ee711c3bad57f3d
Sharad <sharaddargan@gmail.com>
2015-10-31 19:47:07 +0530

    admin panel

--- leave/admin.py
+++ leave/admin.py
@@ -7,7 +7,16 @@ class AttendanceAdmin(admin.ModelAdmin):
 	list_filter = ['date','employee__department']
 	search_fields = ('employee__name',)
 
-admin.site.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+	list_display = ('name','department','leave_count','gender')
+	list_filter = ['department']
+	search_fields = ('name',)
+
+class LeaveAdmin(admin.ModelAdmin):
+	list_display = ('employee','type','start_date','end_date')
+	search_fields = ('employee__name','type')
+
+admin.site.register(Employee,EmployeeAdmin)
 admin.site.register(Attendance,AttendanceAdmin)
 admin.site.register(Department)
-admin.site.register(Leave)
+admin.site.register(Leave,LeaveAdmin)
